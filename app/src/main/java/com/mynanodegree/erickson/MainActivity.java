bf38856aa5db6564fea8af15eeec61387a2eb23a
erickson <ericksondyeggo@gmail.com>
2016-12-18 17:59:54 -0200

    Mockup for buttons

--- app/src/main/java/com/mynanodegree/erickson/MainActivity.java
+++ app/src/main/java/com/mynanodegree/erickson/MainActivity.java
@@ -2,6 +2,9 @@ package com.mynanodegree.erickson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mockUpClick(View v) {
+        Button b = (Button) v;
+
+        String message = (String) this.getText(R.string.mockup_message);
+
+        String msg_toast = String.format(message, b.getText().toString());
+
+        Toast.makeText(
+                this
+                , msg_toast
+                , Toast.LENGTH_SHORT
+        ).show();
     }
 }
